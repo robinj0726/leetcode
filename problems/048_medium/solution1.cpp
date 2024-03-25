0697a9db0131890406ab7f1721d27ff3f07e95f8
robinj730 <88188467+robinj730@users.noreply.github.com>
2024-03-25 14:53:14 +0800

    rotate the matrix by 90 degree clockwise

--- problems/048_medium/solution1.cpp
+++ problems/048_medium/solution1.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 using namespace std;
 
+/* 
+  1, 2, 3       1, 4, 7     7, 4, 1
+  4, 5, 6   ->  2, 5, 8  -> 8, 5, 2
+  7, 8, 9       3, 6, 9     9, 6, 3
+*/
 void rotate(vector<vector<int>>& matrix) {
   int n = matrix.size();
+  // iterate over the upper triangular part of the matrix
   for (int i = 0; i < n; i++) {
     for (int j = i; j < n; j++) {
+      // swap the elements along the diagonal line
       swap(matrix[i][j], matrix[j][i]);
     }
   }
